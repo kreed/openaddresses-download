d640d3943f34dd3a2489759f657397dd94e3a439
Tom MacWright <tom@macwright.org>
2014-03-13 12:35:33 -0700

    First

---
+++ cli.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+
+var argv = require('minimist')(process.argv.slice(2)),
+    fs = require('fs'),
+    ProgressBar = require('progress'),
+    connectors = require('./lib/connectors');
+
+var source = argv._[0],
+    output = argv._[1];
+
+if (!source || !output) {
+    throw new Error('usage: openaddress-download FILE.json output');
+}
+
+var parsed = JSON.parse(fs.readFileSync(source, 'utf8'));
+
+if (!parsed.data) {
+    throw new Error('no data included in source');
+}
+
+var type = connectors.byAddress(parsed.data);
+
+if (!type) {
+    throw new Error('no connector found');
+}
+
+connectors[type](parsed, function(err, stream) {
+    if (!argv.silent) showProgress(stream, type);
+    stream.pipe(fs.createWriteStream(output));
+});
+
+function showProgress(stream, type) {
+    var bar;
+    if (type == 'http') {
+        stream.on('response', function(res) {
+            var len = parseInt(res.headers['content-length'], 10);
+            bar = new ProgressBar('  downloading [:bar] :percent :etas', {
+                complete: '=',
+                incomplete: ' ',
+                width: 20,
+                total: len
+            });
+        });
+    } else if (type == 'ftp') {
+        stream.on('size', function(len) {
+            bar = new ProgressBar('  downloading [:bar] :percent :etas', {
+                complete: '=',
+                incomplete: ' ',
+                width: 20,
+                total: len
+            });
+        });
+    }
+    stream.on('data', function(chunk) {
+        if (bar) bar.tick(chunk.length);
+    }).on('end', function() {
+        if (bar) console.log('\n');
+    });
+}
---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports.connectors = require('./lib/connectors');
